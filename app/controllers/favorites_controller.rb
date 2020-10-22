b9e3e43f93caf8a6c256e58832236ca6d94ea991
EC2 Default User <ec2-user@ip-172-31-22-201.ec2.internal>
2020-10-22 08:10:30 +0000

    [Finish]task6

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,13 +5,13 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     favorite = @book.favorites.new(user_id: current_user.id)
     favorite.save
-    redirect_to request.referer
+        # 非同期通信の場合はredirect を削除
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     favorite = @book.favorites.find_by(user_id: current_user.id)
     favorite.destroy
-    redirect_to request.referer
+    # 非同期通信の場合はredirect を削除
   end
 end
